1d7db965ffdec046064c376c7ee977cc863bc98d
panglaojia <43441204+panglaojia@users.noreply.github.com>
2024-03-07 23:39:30 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,8 @@ let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 // 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
 let addresses = [
   'www.visa.com.hk:2096',
-'v1.arvancode.eu.org:443'
+'v1.arvancode.eu.org:443',
+'series-a1.samanehha.co'
 ];
 
 // 设置优选地址api接口
