758eba9a50351b2e2aa7ebe4e26f84874bb4a2fa
panglaojia <43441204+panglaojia@users.noreply.github.com>
2024-03-07 22:54:10 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -6,8 +6,6 @@ let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 // 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
 let addresses = [
   'www.visa.com.hk:2096',
-  'icook.tw:2053',
-  'cloudflare.cfgo.cc'，
 ];
 
 // 设置优选地址api接口
